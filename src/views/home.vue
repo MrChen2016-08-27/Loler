bd9c27dcc8cf8b8ed68e33c49af24f5f6fdda8fa
Kevin Chen <1909916440@qq.com>
2017-09-21 17:37:40 +0800

    add home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="home" >
+		<div></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data (){
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+	#home{
+		
+	}
+</style>
