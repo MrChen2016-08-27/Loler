dbe3eb3f483b2f4fd860185a8278064a0f0ef291
code_chen <1909916440@qq.com>
2017-09-25 00:10:34 +0800

    添加左侧滑动栏目

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="home" >
  		<md-toolbar>
- 			<md-button class="md-icon-button toolbar-opt">
+ 			<md-button @click="toggleLeftSlidenav" class="md-icon-button toolbar-opt">
  				<md-icon>menu</md-icon>
  			</md-button>
-
 			<h2 class="md-title title" style="">英雄联盟</h2> 
-
 			<md-button class="md-icon-button toolbar-opt">
 			  <md-icon>more_vert</md-icon>
 			</md-button>						
  		</md-toolbar>
-
+ 		<md-sidenav md-swipeable class="md-left" ref="leftSidenav" >
+	 		<md-toolbar class="md-large">
+	      <div class="left-header-tool">
+          <md-avatar class="md-large">
+            <img src="../assets/logo.png" alt="People">
+          </md-avatar>
+	      </div>
+	    </md-toolbar>
+      <md-list>
+        <md-list-item>
+          <md-icon>whatshot</md-icon>
+          <span>热门攻略</span>
+            <md-list-expand>
+              <md-list>
+                  <md-list-item class="md-inset">版本热门组合</md-list-item>
+                  <md-list-item class="md-inset">装备搭配指南</md-list-item>
+                  <md-list-item class="md-inset">上分必备技巧</md-list-item>
+              </md-list>
+            </md-list-expand>    
+        </md-list-item>
+        <md-list-item>
+            <md-icon>videogame_asset</md-icon>
+            <span>我的游戏</span>
+            <md-list-expand>
+              <md-list>
+                  <md-list-item class="md-inset">英雄时刻</md-list-item>
+                  <md-list-item class="md-inset">游戏记录</md-list-item>
+              </md-list>
+            </md-list-expand>
+        </md-list-item>        
+        <md-list-item @click="setRouterAddress('/')">
+            <md-icon>record_voice_over</md-icon>
+            <span>我的关注</span>
+        </md-list-item>
+        <md-list-item>
+          <md-icon>favorite</md-icon>
+          <span>我的收藏</span>          
+        </md-list-item>
+      </md-list>
+ 		</md-sidenav>
 		<md-bottom-bar class="bottom-bar" md-shift>
 		  <md-bottom-bar-item md-icon="bubble_chart">资讯</md-bottom-bar-item>
 		  <md-bottom-bar-item md-icon="perm_contact_calendar">英雄</md-bottom-bar-item>
@@ -25,7 +62,19 @@
 export default {
   data () {
     return {
-
+      nowRouter: ''
+    }
+  },
+  methods: {
+    toggleLeftSlidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    setRouterAddress (address) {
+      this.toggleLeftSlidenav()
+      this.$router.push(address)
+    },
+    actionNowAddress () {
+      this.$router.push(this.nowRouter)
     }
   }
 }
